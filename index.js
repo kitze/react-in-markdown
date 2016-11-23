80baab5f7c6b43c085d9642962362e1108b752da
Martin Urban <xurban42@gmail.com>
2016-11-23 16:17:29 +0100

    fix React direct call error - ad. https://facebook.github.io/react/warnings/legacy-factories.html

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import {getPropsObject} from './src/utils';
 import {link} from './src/components';
+import React from 'react';
 
 export const renderCustomComponents = (props, customComponents, customLinkComponent) => {
   const {children, href} = props;
   const foundComponent = customComponents[children[0]];
   if (foundComponent) {
     const propsObject = getPropsObject(href);
-    return foundComponent(propsObject);
+    return React.createElement(foundComponent, propsObject);
   }
-  return customLinkComponent ? customLinkComponent(props) : link(props);
+  return customLinkComponent ? React.createElement(customLinkComponent, props) : React.createElement(link, props);
 }
